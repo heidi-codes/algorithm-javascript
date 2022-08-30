2a832bb99c59e5e0aec096aaa641bf8704f513ba
HeidiEleven <109416973+heidi-codes@users.noreply.github.com>
2022-08-30 23:02:47 +0800

    feat: place-flowers

---
+++ code/array/05-place-flowers.js
@@ -0,0 +1,28 @@
+/**
+ * @lc app=leetcode.cn id=605 lang=javascript
+ * @description Can Place Flowers
+ * @author Heidi Codes
+ */
+
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+
+export default (arr, n) => {
+  let max = 0
+  arr.push(0)
+  for (let i = 0, len = arr.length - 1; i < len; i++) {
+    if (arr[i] === 0) {
+      if (i === 0 && arr[1] === 0) {
+        max++
+        i++
+      } else if (arr[i - 1] === 0 && arr[i + 1] === 0) {
+        max++
+        i++
+      }
+    }
+  }
+  return max >= n
+}
---
+++ test/array/05-place-flowers.test.js
@@ -0,0 +1,15 @@
+/**
+ * @description Can Place Flowers
+ * @author Heidi Codes
+ */
+import flower from '../../code/array/05-place-flowers'
+
+test('flower:[1,0,0,0,1],1', () => {
+  expect(flower([1, 0, 0, 0, 1], 1)).toBe(true)
+})
+test('flower:[1,0,0,0,1],2', () => {
+  expect(flower([1, 0, 0, 0, 1], 2)).toBe(false)
+})
+test('flower:[1, 0, 0, 0, 1, 0, 0], 2', () => {
+  expect(flower([1, 0, 0, 0, 1, 0, 0], 2)).toBe(true)
+})
