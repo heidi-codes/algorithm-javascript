1b9dde177d2941e8b9347858a0ac97823cb85628
HeidiEleven <109416973+heidi-codes@users.noreply.github.com>
2022-08-30 23:24:02 +0800

    gray-code

---
+++ code/array/06-gray-code.js
@@ -0,0 +1,28 @@
+/**
+ * @lc app=leetcode.cn id=89 lang=javascript
+ * @description Gray Code
+ * @author Heidi Codes
+ */
+
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+
+export default (n) => {
+  let make = (n) => {
+    if (n === 1) {
+      return ['0', '1']
+    } else {
+      let prev = make(n - 1)
+      let result = []
+      let max = Math.pow(2, n) - 1
+      for (let i = 0, len = prev.length; i < len; i++) {
+        result[i] = `0${prev[i]}`
+        result[max - i] = `1${prev[i]}`
+      }
+      return result
+    }
+  }
+  return make(n)
+}
---
+++ test/array/06-gray-code.test.js
@@ -0,0 +1,22 @@
+/**
+ * @description Gray Code
+ * @author Heidi Codes
+ */
+
+import grayCode from '../../code/array/06-gray-code'
+
+test('grayCode:1', () => {
+  expect(grayCode(2)).toEqual(['00', '01', '11', '10'])
+})
+test('grayCode:2', () => {
+  expect(grayCode(3)).toEqual([
+    '000',
+    '001',
+    '011',
+    '010',
+    '110',
+    '111',
+    '101',
+    '100'
+  ])
+})
